e798fadab82041d4648172d6d68c276ab0100360
Ryan Lewis <ryan@wpyz.org>
2017-03-24 13:49:38 +0000

    Don't consider any query string parameters in the filename

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const url = require('url')
 const contentType = require('content-type')
 const mime = require('mime-types')
 const getRawBody = require('raw-body')
@@ -8,9 +9,18 @@ const send = micro.send
 
 const images = path.join(__dirname, 'images')
 
+const getFilenameFromRequest = (req) => {
+  const parsedUrl = url.parse(req.url)
+  if (typeof parsedUrl === 'undefined') return send(res, 404, 'Bad request')
+
+  const filename = path.basename(parsedUrl.pathname)
+  if (typeof filename === 'undefined' || !filename.length) return send(res, 400, 'Bad filename')
+
+  return filename
+}
+
 const get = (req, res) => {
-  const filename = path.basename(req.url)
-  if (typeof filename === 'undefined' || !filename.length) return send(res, 404, 'Not found')
+  const filename = getFilenameFromRequest(req)
 
   const file = path.join(images, filename)
   const mimeType = mime.lookup(filename)
@@ -22,20 +32,18 @@ const get = (req, res) => {
   return send(res, 200, data)
 }
 
-
 const post = async (req, res) => {
-  const filename = path.basename(req.url)
-  if (typeof filename === 'undefined' || !filename.length) return send(res, 404, 'Not found')
+  const filename = getFilenameFromRequest(req)
 
   const data = await getRawBody(req, { 
     length: req.headers['content-length'], limit: '10mb',
     encoding: contentType.parse(req).parameters.charset
   })
 
-  const url = `http://${req.headers['host']}${req.url}`
+  const imageUrl = `http://${req.headers['host']}/${filename}`
 
   fs.writeFileSync(path.join(images, filename), data, { encoding: 'binary' })
-  return { url }
+  return { imageUrl }
 }
 
 const server = micro(async (req, res) => {
